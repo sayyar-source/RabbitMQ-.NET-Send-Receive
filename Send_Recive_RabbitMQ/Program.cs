9e74c618f8547561bc7cbddccedc48e59a514cab
eng.sayyar <eng.sayyar@gmail.com>
2020-04-28 00:48:14 +0300

    header

--- Send_Recive_RabbitMQ/Program.cs
+++ Send_Recive_RabbitMQ/Program.cs
@@ -1,5 +1,8 @@
 ﻿using RabbitMQ.Client;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Text;
 
 namespace Send
@@ -14,7 +17,7 @@ namespace Send
             using (var channel = connection.CreateModel())
             {
                 //RabbitMQ provides four types of exchanges: Direct, Fanout, Topic, and Headers
-                channel.ExchangeDeclare("subject", ExchangeType.Topic, false, false, null);
+                channel.ExchangeDeclare("header", ExchangeType.Headers, false, false, null);
                 channel.QueueDeclare(queue: "a",
                                  durable: false,
                                  exclusive: false,
@@ -30,9 +33,24 @@ namespace Send
                                exclusive: false,
                                autoDelete: false,
                                arguments: null);
-                channel.QueueBind("a", "subject","*.manager.*", null);
-                channel.QueueBind("b", "subject", "director.*", null);
-                channel.QueueBind("c", "subject", "master.#", null);
+                var headerBind1 = new Dictionary<string,object>();
+                headerBind1.Add("x-match", "any");
+                headerBind1.Add("key1", "bmp");
+                headerBind1.Add("key2", "mp4");
+                channel.QueueBind("a", "header","",headerBind1);
+
+                var headerBind2 = new Dictionary<string, object>();
+                headerBind2.Add("x-match", "any");
+                headerBind2.Add("key1", "jpg");
+                headerBind2.Add("key2", "mp3");
+                headerBind2.Add("key3", "mpeg");
+                channel.QueueBind("b", "header", "",headerBind2);
+
+                var headerBind3 = new Dictionary<string, object>();
+                headerBind3.Add("x-match", "all");
+                headerBind3.Add("key1", "mkv");
+                headerBind3.Add("key2", "bpm");
+                channel.QueueBind("c", "header", "", headerBind3);
 
                
                 //--- Chat
@@ -45,9 +63,18 @@ namespace Send
                     Console.Write("routingKey : ");
                     Key = Console.ReadLine();
                     var body = Encoding.UTF8.GetBytes(message);
-                    channel.BasicPublish(exchange: "subject",                   
-                                         routingKey: Key,
-                                         basicProperties: null,
+                    var properties = channel.CreateBasicProperties();
+                    var headers = new Dictionary<string, object>()
+                    {
+                        {"key1",Key},
+                        {"key2",Key },
+                        {"key3",Key },
+                        {"time",DateTime.Now.ToShortTimeString() }
+                    };
+                    properties.Headers = headers;
+                    channel.BasicPublish(exchange: "header",                   
+                                         routingKey: "",
+                                         basicProperties: properties,
                                          body: body);
                     Console.WriteLine(" [producer ] Sent {0}", message);
                 }
